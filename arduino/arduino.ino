e3089a628eb89f736584da33f4c20e3728bb1ef6
jonathanjanssens <jonathan.janssens@gmail.com>
2014-04-03 19:19:40 +0100

    now accepts turning the heating on

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,23 +1,45 @@
 #include <Time.h>
 
-int externalPin = 0;
-int corePin = 1;
-int extremitiesPin = 2;
+int externalPin = 2;
+int corePin = 3;
+int extremitiesPin = 4;
+
+int indicatorGround = A5;
+int heatIndicator = 10;
+int serialIndicator = 11;
+
+int incomingByte = 0;
 
 void setup() {
+  
   Serial.begin(9600);
+  Serial.flush();
+  
+  pinMode(serialIndicator, OUTPUT);
+  pinMode(heatIndicator, OUTPUT);
+  pinMode(indicatorGround, OUTPUT);
+  
+  digitalWrite(indicatorGround, LOW); // set as ground
 }
 
 void loop() {
+  
+  incomingByte = Serial.read();
+  
+  if(incomingByte != -1){
+    digitalWrite(heatIndicator, HIGH);
+  }
+  
+  digitalWrite(serialIndicator, HIGH);
 
-  float externalTemp = analogRead(externalPin);
-  float coreTemp = analogRead(corePin);
-  float extremitiesTemp = analogRead(extremitiesPin);
+  float externalTemp = analogRead(externalPin)*(3.3/1024.0);
+  float coreTemp = analogRead(corePin)*(3.3/1024.0);
+  float extremitiesTemp = analogRead(extremitiesPin)*(3.3/1024.0);
 
   char externalTempChar[10];
-  String externalTempAsString;
+  String externalTempString;
   dtostrf(externalTemp,1,2,externalTempChar);
-  externalTempAsString = String(externalTempChar);
+  externalTempString = String(externalTempChar);
 
   char coreTempChar[10];
   String coreTempString;
@@ -30,33 +52,32 @@ void loop() {
   extremitiesTempString = String(extremitiesTempChar);
 
 
-  String output = "{ \"user_id\": 1, \"readings\": { \"s01\": { \"name\": \"head\", \"value\": 0 } }, \"timetamp\": 1395155056 }";
-
   String json = "{\"user_id\": 1,";
-  json = json + "\"readings\": {";
+  json = json + "\"readings\": [";
   // s01
-  json = json + "{ \"s01\": ";
-  json = json + "\"name\" : \"head\",";
+  json = json + "{\"sensor_id\" : \"1\",";
   json = json + "\"value\" :";
-  json = json + externalTempAsString;
-  json = json + "}";
+  json = json + externalTempString;
+  json = json + "},";
   //s02
-  json = json + "{ \"s02\": ";
-  json = json + "\"name\" : \"core\",";
+  json = json + "{\"sensor_id\" : \"2\",";
   json = json + "\"value\" :";
   json = json + coreTempString;
-  json = json + "}";
+  json = json + "},";
   //s03
-  json = json + "{ \"s02\": ";
-  json = json + "\"name\" : \"extremities\",";
+  json = json + "{\"sensor_id\" : \"3\",";
   json = json + "\"value\" :";
   json = json + extremitiesTempString;
   json = json + "}";
   // and timestamps and close json
-  json = json + "}, \"timestamp\":";
+  json = json + "], \"timestamp\": false";
   json = json + "}";
 
   Serial.println(json);
+  
+  digitalWrite(serialIndicator, LOW);
+  
+  Serial.flush();
+  
   delay(10000);
 }
-
